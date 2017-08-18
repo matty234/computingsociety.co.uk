6a96402cb7fb50820c9ccb0b8e031fdbbf5f063a
Dulcie <dulciejackson@gmail.com>
2017-08-18 10:36:19 +0100

    Created Navbar

--- compsoc-website/src/App.vue
+++ compsoc-website/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/ui/Navbar.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar
+  }
 }
 </script>
 
---
+++ compsoc-website/src/components/ui/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <b-navbar toggleable variant="custom" toggle-breakpoint="md" class="navbar">
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+      <b-navbar-brand href="#">CompSoc</b-navbar-brand>
+      <b-nav is-nav-bar>
+        <b-nav-item href="#">Link</b-nav-item>
+      </b-nav>
+    </b-navbar>
+  </div>
+</template>
