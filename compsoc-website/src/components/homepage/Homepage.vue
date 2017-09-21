5081d5d9351c54ac3dbdc04024ff6f9d092c66cc
dulcibel <dulciejackson@gmail.com>
2017-09-21 18:31:05 +0100

    Fixed main page

--- compsoc-website/src/components/homepage/Homepage.vue
+++ compsoc-website/src/components/homepage/Homepage.vue
@@ -1,17 +1,128 @@
 <template>
   <div>
-    <b-jumbotron fluid class="darkbg logo fullscreen">
-      <img src="./csinverted.png">
-      <h1 class="white">Code. Play. Connect.</h1>
+    <b-jumbotron fluid class="darkbg">
+      <img src="./csinverted.png" class="logo fullscreen">
+      <h1 class="white pb-5">Code. Play. Connect.</h1>
       <br />
       <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div class="row">
+        <div class="col">
+          <img src="./rhsuwhite.png">
+        </div>
+      </div>
     </b-jumbotron>
-    <b-jumbotron fluid class="logo fullscreen">
+    <b-jumbotron fluid>
+      <div class="row">
+        <div class="col">
+          <h3>OUR HISTORY</h3>
+          <p>The Computing Society of Royal Holloway, University of London was founded in late 2012. by Emil Tan, a computer science undergraduate in his second year of study. Since then it has grown into a thriving community of computer enthusiasts from all faculties of studies.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h3>AIMS</h3>
+          <p>The Computing Society focuses on improving its members’ skills and aiding their interests by supplementing their degree programs with talks from professionals in industry, working in improving their skills in workshops and tutorials and making friendships and connections that will last long after they’ve finished their degrees.
+          </p>
+          <p>The society aims to make computing interesting and fascinating to everyone, doing this by encouraging young people to program by voluntarily teaching at primary school, hence providing their members with opportunities to work in their communities around the university campus.
+          </p>
+          <p>As part of its mission the Computing Society keeps it members up to date in the latest computing technologies, promoting active discussions in topics from advance information security to basic Java.
+          </p>
+          <p>The Computing Society aims to create a network of enthusiasts, students, academics, and professionals in the field of computing. We set to achieve this goal by encouraging open source collaboration through publications and sharing sessions, participating in regional and international conferences and competitions and creating opportunities for enthusiasts to meet like-minded people.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h3>ACHIEVEMENTS</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <h4>2013/14</h4>
+          <p>Crest Awards – Jingwei Emil Tan, Christian Jank</p>
+          <p>Society of the Year (Nominated)</p>
+          <p>Best Academic Society (Nominated)</p>
+          <p>Best New Society (Nominated)</p>
+        </div>
+        <div class="col-4">
+          <h4>2014/15</h4>
+          <p>
+            Department of the Year
+          </p>
+          <p>
+            Society of the Year
+          </p>
+          <p>
+            Skills and Employability Cup
+          </p>
+          <p>
+            Runnymede Volunteer Award (Community Action’s
+            Volunteering Group of the Year)
+          </p>
+          <p>
+            Crest Award – Filippo Di Paola
+          </p>
+          <p>
+            Freshman of the Year Award – Remi Jahau
+          </p>
+          <p>
+            Runnymede Volunteer Award (Individual Award) – Feargus Pendlebury
+          </p>
+          <p>
+            Best Academic Society (Nominated)
+          </p>
+          <p>
+            RAG Event of the Year (Nominated)
+          </p>
+        </div>
+        <div class="col-4">
+          <h4>2015/16</h4>
+          <p>Freshman of the Year Award – Omoyeme Akhigbe
+          </p>
+        </div>
+      </div>
+    </b-jumbotron>
+    <b-jumbotron fluid class="darkbg">
+      <div class="row">
+        <img src="./fbicon.png" class="icon">
+        <img src="./twittericon.png" class="icon">
+        <img src="./liicon.png" class="icon">
+        <img src="./rhullogo.jpg" class="icon right">
+      </div>
+      <div class="row">
+        <div class="col">
+          <p class="white">Registered Charity 1141998</p>
+        </div>
+      </div>
     </b-jumbotron>
   </div>
 </template>
 
 <style>
+
+.right {
+  position: absolute;
+  right: 0px;
+}
+
+.icon {
+  height: 50px;
+  width: auto;
+}
+
+.fillcol {
+  width: inherit;
+}
+
+.redbg {
+  background-color: #cc5544
+}
+
 .darkbg {
   background-color: #282423
 }
