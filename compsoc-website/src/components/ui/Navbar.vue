9156a7985ebaa493e738fca564537f877e04e1b8
dulcibel <dulciejackson@gmail.com>
2017-09-27 14:12:04 +0100

    Hide nav

--- compsoc-website/src/components/ui/Navbar.vue
+++ compsoc-website/src/components/ui/Navbar.vue
@@ -5,7 +5,7 @@
       <b-navbar-brand :to="{name: 'Home'}">
         <img src="./csinverted.png" class="d-inline-block align-top small">
       </b-navbar-brand>
-      <b-nav is-nav-bar>
+      <!--<b-nav is-nav-bar>
         <b-nav-item href="#">Socials</b-nav-item>
         <b-nav-item href="#">Career Opportunities</b-nav-item>
         <b-nav-item href="#">Girls Who Code</b-nav-item>
@@ -13,7 +13,7 @@
         <b-nav-item href="#">Volunteering</b-nav-item>
         <b-nav-item href="#">Workshops</b-nav-item>
         <b-nav-item href="#">Calendar</b-nav-item>
-      </b-nav>
+      </b-nav>-->
     </b-navbar>
   </div>
 </template>
