7c463dd87d15bf1750220f1f69b920949d1d68bd
dulcibel <dulciejackson@gmail.com>
2018-03-20 15:04:15 +0000

    :sparkles: Added Royal Hackaway Link

--- compsoc-website/src/components/ui/Navbar.vue
+++ compsoc-website/src/components/ui/Navbar.vue
@@ -5,15 +5,16 @@
       <b-navbar-brand :to="{name: 'Home'}">
         <img src="./csinverted.png" class="d-inline-block align-top small">
       </b-navbar-brand>
-      <!--<b-nav is-nav-bar>
-        <b-nav-item href="#">Socials</b-nav-item>
+      <b-nav is-nav-bar>
+        <b-nav-item href="http://royalhackaway.com/">Royal Hackaway</b-nav-item>
+        <!--<b-nav-item href="#">Socials</b-nav-item>
         <b-nav-item href="#">Career Opportunities</b-nav-item>
         <b-nav-item href="#">Girls Who Code</b-nav-item>
         <b-nav-item href="#">Charity</b-nav-item>
         <b-nav-item href="#">Volunteering</b-nav-item>
         <b-nav-item href="#">Workshops</b-nav-item>
-        <b-nav-item href="#">Calendar</b-nav-item>
-      </b-nav>-->
+        <b-nav-item href="#">Calendar</b-nav-item>-->
+      </b-nav>
     </b-navbar>
   </div>
 </template>
