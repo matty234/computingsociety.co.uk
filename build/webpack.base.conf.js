e0ba4ebd72d2d07188f53bcdf8517660fd2c03ff
Benzi Mooneegan <hi@benzi.io>
2018-03-25 15:58:25 +0100

    hot update spammy reload fix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -14,9 +14,9 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath: '/',
+    hotUpdateChunkFilename: 'hot/hot-update.js',
+    hotUpdateMainFilename: 'hot/hot-update.json'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
