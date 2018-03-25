a17fff3cf91358578cfe9b20cdac53b4dd8f1a23
Benzi Mooneegan <hi@benzi.io>
2018-03-25 16:25:45 +0100

    test

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -19,12 +19,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     })
   },
   devtool: config.build.productionSourceMap ? '#source-map' : false,
-  output: {
-    path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
-  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
